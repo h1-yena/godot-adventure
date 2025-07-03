604e4e59608d5bdc095bc46f38c86fc98731cc81
Sebastian <github.constant352@passmail.net>
2025-07-03 23:24:47 +0200

    made dialog box appear and disappear on interact button press!

--- Scenes/NPC/npc.gd
+++ Scenes/NPC/npc.gd
@@ -4,4 +4,10 @@ extends StaticBody2D
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("interact"):
-		print(self.name + ": Waow, hewwo! :3")
+		
+		print(self.name + ": Action pressed!")
+		
+		if $DialogCanvas.visible == false:
+			$DialogCanvas.visible = true
+		else:
+			$DialogCanvas.visible = false
