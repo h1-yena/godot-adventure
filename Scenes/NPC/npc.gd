af89396fb6baeaf071e27bc6bd2a8a5a4621bc6b
Sebastian <github.constant352@passmail.net>
2025-07-06 00:03:05 +0200

    added dialog lines array

--- Scenes/NPC/npc.gd
+++ Scenes/NPC/npc.gd
@@ -1,6 +1,12 @@
 extends StaticBody2D
 
 var can_interact: bool = false
+var dialog_lines: Array[String] = ["Hello", "What\'s up?", "Sayonara!"]
+
+func _ready() -> void:
+	
+	for value in dialog_lines:
+		print(value)
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("interact") and can_interact:
@@ -9,5 +15,7 @@ func _process(delta: float) -> void:
 		
 		if $DialogCanvas.visible:
 			$DialogCanvas.visible = false
+			get_tree().paused = false
 		else:
 			$DialogCanvas.visible = true
+			get_tree().paused = true
