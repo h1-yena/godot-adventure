686e501018f7cb9f0f57d4c5f65403105a60883f
Sebastian <github.constant352@passmail.net>
2025-07-06 01:05:17 +0200

    added world boundary and npc dialog!

--- Scenes/NPC/npc.gd
+++ Scenes/NPC/npc.gd
@@ -1,21 +1,27 @@
 extends StaticBody2D
 
 var can_interact: bool = false
-var dialog_lines: Array[String] = ["Hello", "What\'s up?", "Sayonara!"]
-
-func _ready() -> void:
-	
-	for value in dialog_lines:
-		print(value)
+@export var dialog_lines: Array[String] = ["Hello", "What\'s up?", "Sayonara!"]
+var dialog_index: int = 0
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("interact") and can_interact:
 		
-		print(self.name + ": Action pressed!")
+		$DialogCanvas.visible = true
+		get_tree().paused = true
 		
-		if $DialogCanvas.visible:
+		
+		if dialog_index < dialog_lines.size()-1:
+			$DialogCanvas/DialogBox/DialogText.text = dialog_lines[dialog_index]
+			dialog_index += 1 
+		else:
+			$DialogCanvas/DialogBox/DialogText.text = dialog_lines[dialog_index]
 			$DialogCanvas.visible = false
 			get_tree().paused = false
-		else:
-			$DialogCanvas.visible = true
-			get_tree().paused = true
+			dialog_index = 0
+		
+
+		#if dialog_index == dialog_lines.size() - 1:
+		#	$DialogCanvas.visible = false
+		#	get_tree().paused = false
+						
